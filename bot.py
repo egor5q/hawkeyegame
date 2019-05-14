e1bd1edd60c6895882434611672cc6a99e1fd8ea
egor5q <31898567+egor5q@users.noreply.github.com>
2019-05-14 20:00:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,7 +101,8 @@ def fight():
     t=threading.Timer(btimer, fight)
     t.start()
     
-        
+fight() 
+
 def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
     return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
                                  parse_mode=parse_mode)   
