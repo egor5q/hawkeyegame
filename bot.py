3f3b86597fd208b3c2bfff66a6b7c11085b3f684
egor5q <31898567+egor5q@users.noreply.github.com>
2019-05-14 19:55:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,12 +12,96 @@ import traceback
 token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 
-
-client=MongoClient(os.environ['database'])
-db=client.
-users=db.users
+fighters=[]
+btimer=30
 
 
+@bot.message_handler(commands=['start'])
+def start(m):
+    if m.from_user.id not in fighters:
+        fighters.append(createplayer(m.from_user))
+        bot.send_message(m.chat.id, 'Вы успешно зашли в игру! Теперь ждите, пока ваш боец прострелит кому-нибудь яйцо.\nСоветую кинуть бота в мут!')
+     
+    
+@bot.message_handler(commands=['settimer'])
+def settimer(m):
+    if m.from_user.id==441399484:
+        try:
+            global btimer
+            btimer=int(m.text.split(' ')[1])
+        except:
+            pass
+        
+@bot.message_handler(commands=['stats'])
+def stats(m):
+    me=None
+    for ids in fighters:
+        if ids['id']==m.from_user.id:
+            me=ids
+    if me!=None:
+        text=''
+        text+='ХП: '+str(me['hp'])+'\n'
+        text+='В вас попали: '+str(me['hitted'])+' раз(а)\n'
+        text+='Вы убили: '+str(me['killed'])+' дурачков\n'
+        bot.send_message(m.chat.id, text)
+
+
+def createplayer(user):
+    return {
+        'hp':1000,
+        'damage':10,
+        'killchance':5,
+        'name':user.first_name,
+        'id':user.id,
+        'hitted':0,  # сколько раз попали
+        'killed':0,   # сколько уебал
+        'killer':''
+           }
+           
+   
+    
+def fight():
+    for ids in fighters:
+        alive=[]
+        for idss in fighters:
+            if idss['hp']>0 and idss['id']!=ids['id']:
+                alive.append(idss)
+        if len(alive)>0:
+            text=''
+            target=random.choice(alive)
+            dmg=ids['damage']+ids['damage']*(random.randint(-20, 20)/100)
+            target['hp']-=dmg
+            target['hitted']+=1
+            text+='Вы попали в '+target['name']+'! Нанесено '+str(dmg)+' урона.\n'
+            if target['hp']<=0:
+                ids['killed']+=1
+                target['killer']=ids['name']
+                text+='Вы убили цель!\n'
+            else:
+                if random.randint(1,100)<=ids['killchance']:
+                    target['hp']=0
+                    ids['killed']+=1
+                    target['killer']=ids['name']
+                    text+='Вы прострелили яйцо цели! Та погибает.\n'
+            bot.send_message(ids['id'], text)
+    dellist=[]
+    for ids in fighters:
+        if ids['hp']<=0:
+            dellist.append(ids)
+    for ids in dellist:
+        bot.send_message(ids['id'], 'Вы сдохли. Вас убил '+ids['killer'])
+        me=ids
+        text='Итоговые статы:\n\n'
+        text+='ХП: '+str(me['hp'])+'\n'
+        text+='В вас попали: '+str(me['hitted'])+' раз(а)\n'
+        text+='Вы убили: '+str(me['killed'])+' дурачков\n'
+        bot.send_message(ids['id'], text)
+        fighters.remove(ids)
+    global btimer
+    t=threading.Timer(btimer, fight)
+    t.start()
+    
+        
 def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
     return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
                                  parse_mode=parse_mode)   
